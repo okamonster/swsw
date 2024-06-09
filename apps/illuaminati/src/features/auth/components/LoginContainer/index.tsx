077947048e8a8b6a01db7b71d3c29f4783a7014a
okamonster <okamonster0615@gmail.com>
2024-06-10 05:20:39 +0900

    feat:add logincontainer

---
+++ apps/illuaminati/src/features/auth/components/LoginContainer/index.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+import styles from './style.module.css'
+
+import { LoginForm } from '~/features/auth/components//LoginForm'
+
+export const LoginContainer = (): React.ReactNode => {
+  return (
+    <div className={styles.loginContainer}>
+      <Image
+        src="/images/svgs/swsw_logo.svg"
+        width={300}
+        height={150}
+        alt="スワンスワンズ"
+        className={styles.logo}
+      />
+      <h1 className={styles.title}>ログイン</h1>
+      <LoginForm />
+    </div>
+  )
+}
