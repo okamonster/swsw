88482f2f2717ed5d7ca61dc7853d883a83efc0bc
okamonster <okamonster0615@gmail.com>
2024-06-10 03:18:31 +0900

    feat:add toppage actions

--- apps/illuaminati/src/features/top/TopContainer/index.tsx
+++ apps/illuaminati/src/features/top/TopContainer/index.tsx
@@ -1,5 +1,37 @@
+import Image from 'next/image'
+
 import styles from './style.module.css'
 
+import { LinkButton } from '~/components/LinkButton'
+
 export const TopContainer = (): React.ReactNode => {
-  return <div className={styles.topContainer}></div>
+  return (
+    <div className={styles.topContainer}>
+      <div className={styles.actions}>
+        <Image
+          src="/images/svgs/swsw_logo.svg"
+          width={300}
+          height={150}
+          alt="スワンスワンズ"
+          className={styles.logo}
+        />
+
+        <LinkButton
+          label="ログイン"
+          width="300px"
+          variant="secondary"
+          radius="lg"
+          href="login"
+        />
+
+        <LinkButton
+          label="会員証をつくる"
+          width="300px"
+          variant="secondary"
+          radius="lg"
+          href="signup"
+        />
+      </div>
+    </div>
+  )
 }
