b56b7def8e0e01d932bbd02e564ec810dfbf118d
okamonster <okamonster0615@gmail.com>
2024-06-10 05:20:26 +0900

    feat:add signupForm

---
+++ apps/illuaminati/src/features/auth/components/SignupForm/index.tsx
@@ -0,0 +1,55 @@
+import { useForm } from 'react-hook-form'
+import { PasswordInput, TextInput } from '@mantine/core'
+import Link from 'next/link'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useCallback } from 'react'
+
+import styles from './style.module.css'
+
+import { BaseButton } from '~/components/BaseButton'
+import { LinkButton } from '~/components/LinkButton'
+import type { SignupFormSchemaType } from '~/features/auth/types'
+import { signupFormSchema } from '~/features/auth/types'
+
+export const SignupForm = (): React.ReactNode => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignupFormSchemaType>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: zodResolver(signupFormSchema),
+    mode: 'onSubmit',
+  })
+
+  const onSubmit = useCallback(() => {}, [])
+
+  return (
+    <form className={styles.signupForm} onSubmit={handleSubmit(onSubmit)}>
+      <TextInput
+        {...register('email')}
+        placeholder="メールアドレス"
+        label="メールアドレス"
+        error={errors.email?.message}
+      />
+      <PasswordInput
+        {...register('password')}
+        placeholder="パスワード"
+        label="パスワード"
+        error={errors.password?.message}
+        type="password"
+      />
+
+      <div className={styles.actions}>
+        <BaseButton label="登録" radius="lg" />
+        <LinkButton label="もどる" href="/" radius="lg" variant="tertiary" />
+        <Link href="/login" className={styles.loginLink}>
+          ログイン
+        </Link>
+      </div>
+    </form>
+  )
+}
