d9795e27f088e2abc574e04d347b5a1a0e152084
okamonster <okamonster0615@gmail.com>
2024-06-11 03:19:00 +0900

    feat:add useCreateUserMutation

--- apps/illuaminati/src/infrastructure/operations/UserOperations.ts
+++ apps/illuaminati/src/infrastructure/operations/UserOperations.ts
@@ -1,8 +1,15 @@
-import { doc, getDoc } from 'firebase/firestore'
+import { doc, getDoc, setDoc } from 'firebase/firestore'
 
 import { db } from '~/libs/firebase'
-import type { UserId } from '~/types/entities/User'
+import type { CreateUserDto, UserId } from '~/types/entities/User'
 import { userCollection } from '~/types/entities/User'
+
+export const createUserOperation = async (
+  userId: UserId,
+  dto: CreateUserDto,
+): Promise<void> => {
+  await setDoc(doc(db, userCollection, userId), { ...dto })
+}
 
 export const isExistUserByIdOperation = async (userId: UserId) => {
   const user = await getDoc(doc(db, userCollection, userId))
