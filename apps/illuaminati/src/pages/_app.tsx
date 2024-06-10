5f03b9069e309e910bdcb59f602682a5d8a2e2ea
okamonster <okamonster0615@gmail.com>
2024-06-10 15:18:54 +0900

    feat:add notifications

--- apps/illuaminati/src/pages/_app.tsx
+++ apps/illuaminati/src/pages/_app.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable import/order */
 /* eslint-disable @typescript-eslint/naming-convention */
 import { MantineProvider } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
 import type { AppProps } from 'next/app'
 
 // eslint-disable-next-line import/order
@@ -9,13 +11,14 @@ import '~/styles/globals.css'
 import '~/styles/reset.css'
 import '~/styles/variables.css'
 
-// eslint-disable-next-line import/order
 import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <MantineProvider>
+        <Notifications position="top-center" />
         <Component {...pageProps} />
       </MantineProvider>
     </AuthProvider>
