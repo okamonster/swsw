92a047907db29aaf2e3d4b774013ba20dee1f990
okamonster <okamonster0615@gmail.com>
2024-06-22 00:12:04 +0900

    fix:add profile admin

---
+++ apps/illuaminati/src/features/adminUser/profile/components/RegisterContainer/index.tsx
@@ -0,0 +1,12 @@
+import styles from './style.module.css'
+
+import { ProfileForm } from '~/features/generalUser/profile/components/ProfileForm'
+
+export const RegisterContainer = (): React.ReactNode => {
+  return (
+    <div className={styles.registerContainer}>
+      <h1 className={styles.title}>プロフィール登録</h1>
+      <ProfileForm />
+    </div>
+  )
+}
