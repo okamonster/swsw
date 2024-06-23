45dd802313de822caa56409a0940e6bf9508a556
okamonster <okamonster0615@gmail.com>
2024-06-23 10:32:03 +0900

    feat:add sns list

--- apps/illuaminati/src/features/home/components/SwanswansCard/index.tsx
+++ apps/illuaminati/src/features/home/components/SwanswansCard/index.tsx
@@ -7,6 +7,7 @@ import styles from './style.module.css'
 
 import type { AdminUser } from '~/types/entities/AdminUser'
 import { useAdminUserType } from '~/features/adminUser/profile/hooks/useUserType'
+import { IconButton } from '~/components/IconButton'
 
 type Props = {
   user: AdminUser
@@ -32,6 +33,34 @@ export const SwanswansCard = ({ user }: Props): React.ReactNode => {
         <div className={styles.profileDetail}>
           <p className={styles.displayName}>{user.displayName}</p>
           {user.comment && <p className={bubbleClass}>{user.comment}</p>}
+        </div>
+      </div>
+      <div className={styles.actions}>
+        <div className={styles.snsList}>
+          <Link
+            href={`https://x.com/${user.twitterId}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconButton
+              icon={<FaXTwitter size={20} color="var(--color-text-white)" />}
+              backgroundColor="var(--bg-color-black)"
+              onClick={() => {}}
+              size={40}
+            />
+          </Link>
+          <Link
+            href={`https://www.instagram.com/${user.instagramId}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconButton
+              icon={<FaInstagram size={20} color="var(--color-text-white)" />}
+              backgroundColor="#DD2A7B"
+              onClick={() => {}}
+              size={40}
+            />
+          </Link>
         </div>
       </div>
     </div>
