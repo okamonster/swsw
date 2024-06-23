7d1625e25d7bb77d15a5a43a61c5958cb9898063
okamonster <okamonster0615@gmail.com>
2024-06-23 09:42:33 +0900

    feat:add home

--- apps/illuaminati/src/pages/home.tsx
+++ apps/illuaminati/src/pages/home.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 
 import { DefaultLayout } from '~/Layouts/DefaultLayout'
+import { HomeContainer } from '~/features/home/components/HomeContainer'
 
 const Page: NextPage = () => {
   return (
     <DefaultLayout currentTab="home">
-      <div />
+      <HomeContainer />
     </DefaultLayout>
   )
 }
--- apps/illuaminati/src/pages/swanswansAdmin/home.tsx
+++ apps/illuaminati/src/pages/swanswansAdmin/home.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 
 import { AdminLayout } from '~/Layouts/AdminLayout'
+import { HomeContainer } from '~/features/home/components/HomeContainer'
 
 const Page: NextPage = () => {
   return (
     <AdminLayout currentTab="home">
-      <div />
+      <HomeContainer />
     </AdminLayout>
   )
 }
