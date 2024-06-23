fecb215010fcb04713cf91cff822dd4383198544
okamonster <okamonster0615@gmail.com>
2024-06-23 18:06:51 +0900

    fix import

--- apps/illuaminati/src/pages/_app.tsx
+++ apps/illuaminati/src/pages/_app.tsx
@@ -7,8 +7,8 @@ import type { AppProps } from 'next/app'
 // eslint-disable-next-line import/order
 import { AuthProvider } from '~/providers/AuthProvider'
 
-import '~/styles/globals.css'
 import '~/styles/reset.css'
+import '~/styles/globals.css'
 import '~/styles/variables.css'
 import '~/styles/fonts.css'
 
