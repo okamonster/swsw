09b62dc5635fdcb17a591b0c126239847ce41950
okamonster <okamonster0615@gmail.com>
2024-06-27 01:07:46 +0900

    fix:eslint version

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -14,7 +14,6 @@ module.exports = {
     'plugin:import/typescript',
     'prettier',
     require.resolve('@vercel/style-guide/eslint/next'),
-    'eslint-config-turbo',
   ],
   globals: {
     React: true,
