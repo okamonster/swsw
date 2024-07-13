88c190cceb18c3fb329f5e54f360219d57beba8c
okamonster <okamonster0615@gmail.com>
2024-07-13 09:16:39 +0900

    fix:edge runtime

--- apps/illuaminati/src/pages/swsw/[id].tsx
+++ apps/illuaminati/src/pages/swsw/[id].tsx
@@ -2,6 +2,7 @@ import type { GetServerSideProps, NextPage } from 'next'
 
 import { ShareLayout } from '~/Layouts/ShareLayout'
 import { UserDetailContainer } from '~/features/adminUser/detail/UserDetailContainer'
+export const runtime = 'experimental-edge'
 
 type Props = {
   username: string
